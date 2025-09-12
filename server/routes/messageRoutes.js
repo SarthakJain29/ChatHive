47c2f6afdd7ddcca228d057be47ee547c10b6ae2
SarthakJain29 <sarthakjain2907@gmail.com>
2025-09-12 11:09:32 +0530

    message route

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -4,12 +4,12 @@ import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from 
 
 const messageRouter = express.Router();
 
-messageRouter.get("/users", protectRoute, getUsersForSidebar);
+messageRouter.get("/users", protectRoute, getUsersForSidebar); //sidebar users
 
-messageRouter.get("/:id", protectRoute, getMessages);
+messageRouter.get("/:id", protectRoute, getMessages);   //messages by id
 
-messageRouter.put("mark/:id", protectRoute, markMessageAsSeen);
+messageRouter.put("mark/:id", protectRoute, markMessageAsSeen);   //marking messages as seen
 
-messageRouter.post("/send/:id", protectRoute, sendMessage);
+messageRouter.post("/send/:id", protectRoute, sendMessage);  //sending messages
 
 export default messageRouter;
