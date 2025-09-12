a61d45dc9daf6a180b22816a28e469ebd4725b40
SarthakJain29 <sarthakjain2907@gmail.com>
2025-09-12 10:12:29 +0530

    setting up socket.io

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,9 +1,10 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
-
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 //get all users except logged in user
-const getUsersForSidebar = async (req, res) => {
+export const getUsersForSidebar = async (req, res) => {
     try {
         const userId = req.user._id;
         const filteredUsers = await User.find({_id: {$ne: userId}}).select("-password") //displaying all users in leftbar except us
@@ -23,4 +24,70 @@ const getUsersForSidebar = async (req, res) => {
         console.log(error.message);
         res.json({success: true, message: error.message});
     }
+}
+
+
+//get all messages for selected user
+export const getMessages = async (req, res) => {
+    try {
+        const {id: selectedUserId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId} //for getting msgs bw 2 users
+            ]
+        })
+        await Message.updateMany({senderId: selectedUserId, receiverId: myId}, {seen: true}); //marking messages as seen
+
+        res.json({success:true, messages});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: true, message: error.message});
+    }
+}
+//to mark message as seen using message id
+export const markMessageAsSeen = async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true});
+        res.json({success: true});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+//send message to selected user
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const {receiverId} = req.params.id;
+        const senderId = req.user._id; 
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image); //uploading image
+            imageUrl = uploadResponse.secure_url; //getting its url
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })//adding message data to db
+
+        //emit the new message to receiver's socket
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+           io.to(receiverSocketId).emit("newMessage", newMessage); 
+        }
+
+        res.json({success:true, newMessage});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }   
 }
--- server/server.js
+++ server/server.js
@@ -4,10 +4,38 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import {Server} from "socket.io";
+import { log } from "console";
 
 //creating express app and http server
 const app = express();
 const server = http.createServer(app);
+
+//initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"} //allowing every origin
+})
+
+//store online users
+export const userSocketMap = {}  // { userId : socketId}
+
+//Socket.io connection handler
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+    
+    //emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("user Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
 
 //middlware
 app.use(express.json({limit: "4mb"})); //for json parsing
@@ -17,7 +45,8 @@ app.use(cors()) //for cross origin
 app.use("/api/status", (req,res) => {
     res.send("Server is live");
 })
-app.use("/api/auth", userRouter);
+app.use("/api/auth", userRouter); //user route
+app.use("/api/messages", messageRouter); //messages route
 
 //connect to mongodb
 await connectDB();
